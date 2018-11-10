6e922afe98a8cea1114be218e9bec3cc9e967771
soyket <soyket.handymama@gmail.com>
2018-11-10 22:24:20 +0600

    add jwt

--- app.js
+++ app.js
@@ -9,4 +9,44 @@ app.get('/api', (req, res) => {
     });
 });
 
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err){
+            res.json({
+                message: 'token invalid'
+            });
+        }else {
+            res.json({
+                message: 'Post created....'
+            });
+        }
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    const user = {
+        id: 1,
+        username: 'soyket',
+        email: 'soyket@gmail.com'
+    }
+    jwt.sign({ user }, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+        res.json({
+            token
+        })
+    });
+});
+
+// verify token
+function verifyToken(req, res, next){
+    const userToken = req.headers['authtoken'];
+    if (typeof userToken !== undefined){
+        const token = userToken.split(' ');
+        const authToken = token[1];
+        req.token = authToken;
+        next();
+    }else {
+        res.status(403);
+    }
+};
+
 app.listen(5000, () => console.log('server start on port 5000'));
